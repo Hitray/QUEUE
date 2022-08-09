280035a511b297cc161566513c65f0ed1382753a
Hitray <75577433+Hitray@users.noreply.github.com>
2022-08-09 09:04:43 +0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private final String name;
+    private final String surName;
+    private int numberOfTickets;
+
+    public Person(String name, String surName) {
+        this.name = name;
+        this.surName = surName;
+        this.numberOfTickets = 0;
+    }
+
+    public void setNumberOfTickets(int numberOfTickets) {
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    public int getNumberOfTickets() {
+        return numberOfTickets;
+    }
+
+    public void decrementNumberOfTickets() {
+        if (numberOfTickets > 0) {
+            numberOfTickets--;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
 }
